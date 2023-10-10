07110766f5f7b0474cbd06cc3800816d0e28553e
fathimamirz <126368541+fathimamirz@users.noreply.github.com>
2023-10-10 13:21:31 +0530

    Create MainActivity.java

---
+++ SEM_3/ANDROID/Q1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mirza1;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.mirza1.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText user,pwd;
+    String u="s6108";
+    String p="abc";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void verify(View v)
+    {
+        if(v.getId()==R.id.loginbutton){
+            user=(EditText)
+                    findViewById(R.id.username);
+            pwd=(EditText)
+                    findViewById(R.id.password);
+            String a=user.getText().toString();
+            String b=pwd.getText().toString();
+            if(a.equals(u) && b.equals(p))
+                    Toast.makeText(this,"login Succesful",Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(this,"Invalid login",Toast.LENGTH_LONG).show();
+        }
+    }
+}
